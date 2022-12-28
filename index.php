4917ad378d73d7f6be2cdd04473cfc29e1f60ce9
saturnino2022 <121139331+saturnino2022@users.noreply.github.com>
2022-12-28 17:46:30 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+
+<html lang="en">
+<?php include("controller.php"); ?>
+ <head> 
+  <link rel="stylesheet prefetch" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
+   
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twbs-pagination/1.4.1/jquery.twbsPagination.min.js"></script>
+  <style type="text/css" >
+   .wrapper{
+   width:100%;
+    margin: auto;
+   text-align: center;
+   }
+
+   @media(min-width:970px){
+    .wrapper{
+       width:100%;
+    }
+}
+   #pagination-demo{
+   display: inline-block;
+   margin-bottom: 1.75em;
+   }
+   #pagination-demo li{
+   display: inline-block;
+   }
+   
+            #post-data{
+            width: 500px; border: 2px dotted #d6d7da; 
+            padding: 0px 15px 15px 15px; 
+            border-radius: 5px;font-family: arial; 
+            line-height: 16px;color: #333333; font-size: 14px; 
+            background: #ffffff;rgba(200,200,200,0.7) 0 4px 10px -1px;
+            margin: 10px auto;
+            }
+
+   
+  </style>
+ </head>
+ <body>
+
+  <div id="load-data"> </div>
+  
+  <span id="loading"  style="position:relative;left:680px;"> <img src="loading.gif"  /> </span>
+      
+                              
+  <div class="wrapper">        
+   <ul id="pagination-demo" ></ul> 
+  </div>
+  
+  <script>
+   $(document).ready(function() { 
+    
+    $('#pagination-demo').twbsPagination({
+     totalPages: <?php $a = new paginationClass(); echo $a->getTotalNumberOfRecords();  ?>,
+     visiblePages: 4,
+     next: 'Siguiente',
+     prev: 'anterior',
+     onPageClick: function (event, page) {           
+      loadData(page-1);
+     }
+    });
+    
+    function loadData(pageno){
+     
+     //ajax request 
+     dataString="pageNo="+ pageno ;
+     
+     $.ajax({
+      type: "POST",
+      url: "controller.php" ,
+      data: dataString,
+      cache: false,
+      beforeSend: function(){   
+       $( "#loading" ).show();      
+      },
+    
+     }); 
+     
+    }
+    loadData(0) ;
+    
+   }); 
+   
+  </script>  
+ </body>
+</html>
